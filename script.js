79fc1451c25d9b5c7e64c9fb863db510d1cc5ad3
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-17 18:37:16 -0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-
+/* Exemplo de como aparecer mensagem no console do navegador:
 console.log("Hello World");
-console.log("Hello World Again");
+console.log("Hello World Again"); */
+
+var nome = "Ingrid";
+nome = "Ingrid de Souza";
+console.log(nome);
+
+
+
+
+
+
+
+
+/* var = variável; 
+console.log = imprimir variável no console do navegador, se colocar outro valor para "nome" 
+ele irá substituir o anterior;
+string = informações de texto( tudo que é texto é string), posso colocar aspas simples ou duplas;
+*/
