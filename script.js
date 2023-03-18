e19c38d57e11460755768f1f805e5108d90a119d
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-17 23:25:50 -0300

    Update

--- script.js
+++ script.js
@@ -5,18 +5,28 @@ console.log("Hello World Again"); */
 //var idade = 9*2;
 //var nome = "Ingrid";
 
-let idade = 9*2;
-let nome = "Ingrid";
+//let idade = 9*2;
+//let nome = "Ingrid";
 
-let mensagem = (`Feliz aniversário ${nome}!`);
+//let mensagem = (`Feliz aniversário ${nome}!`);
 
-idade = idade + 1; // posso alterar o valor de idade
+//idade = idade + 1; // posso alterar o valor de idade
 //nome = "Ingrid de Souza";
-console.log("Oiii " + nome + "!");
-console.log("Tenho " + idade);
-console.log(mensagem);
+//console.log("Oiii " + nome + "!");
+//console.log("Tenho " + idade);
+//console.log(mensagem);
 
+/*if(idade >= 18) {
+    console.log("Bem vindx!");
+} else { console.log("Acesso proibido"); }*/
 
+//console.log(typeof nome);
+
+let pessoa = {
+    nome: "Ingrid",
+    idade: 18,
+};
+console.log(pessoa.nome);
 
 
 
@@ -31,5 +41,9 @@ ele irá substituir o anterior;
 string = informações de texto( tudo que é texto é string), posso colocar aspas simples ou duplas;
 posso fazer mensagens concatenadas, ex: console.log("Hello " + variável");
 &{} = concatenação de strings, ex: console.log(`Hello ${variável}`);
+if = se, ex: if (idade >= 18) {console.log("Maior de idade");} else {console.log("Menor de idade");};
+typeof = tipo de variável, ex: console.log(typeof variável), ele imprime no console o tipo da variável no caso 
+do ex: "nome" aparece string;
+addEventListener = adicionar um evento, ex: addEventListener("click", function(){console.log("Hello World");});
 
 */
