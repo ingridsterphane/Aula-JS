865f32417456ebd1684ad6ca478022d4efe053de
ingridsterphane <ingridsterphane@hotmail.com>
2023-03-17 21:59:27 -0300

    Update

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ console.log("Hello World Again"); */
 //var nome = "Ingrid";
 
 let idade = 9*2;
-const nome = "Ingrid";
+let nome = "Ingrid";
+
+let mensagem = (`Feliz aniversário ${nome}!`);
 
 idade = idade + 1; // posso alterar o valor de idade
 //nome = "Ingrid de Souza";
-console.log(nome);
-console.log(idade);
+console.log("Oiii " + nome + "!");
+console.log("Tenho " + idade);
+console.log(mensagem);
+
 
 
 
@@ -25,5 +29,7 @@ let = variável, let aceita numeros ex: 5x4;
 console.log = imprimir variável no console do navegador, se colocar outro valor para "nome" 
 ele irá substituir o anterior;
 string = informações de texto( tudo que é texto é string), posso colocar aspas simples ou duplas;
+posso fazer mensagens concatenadas, ex: console.log("Hello " + variável");
+&{} = concatenação de strings, ex: console.log(`Hello ${variável}`);
 
 */
